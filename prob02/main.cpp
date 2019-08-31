2ea6dc55ab0881f80efce7c0dad3eef9546beca3
willyr2501 <willyr2501@cs104u-3a.acad.fullerton.edu>
2019-08-31 15:09:13 -0700

    short description of code changes being uploaded

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -1,21 +1,21 @@
-// Name: 
+// Name: Willy Reyes-Rendon
 // This program calculates the area of a room.
 
 #include <iostream>
 
 int main()
 {
-  double width, length;
-  int area;
+  double width, length, area;
+  width=11.5;
+  length=17.8;
 
   // Get the length and width of the room.
-  std::cout << "What is the room's "width" in feet? ";
+  std::cout << "What is the room's \"width\" in feet? ";
   std::cin >> width;
-  std::cout << "What is the room's "length" in feet? ";
-  std::cin >> area;
-
+  std::cout << "What is the room's \"length\" in feet? ";
+  std::cin >> length;
   // Calculate the area of the room.
-  area = length / width;
+  area = length * width;
 
   // Display the area of the room.
   std::cout << "The \"area\" of the room is 204.7 square feet./n";
