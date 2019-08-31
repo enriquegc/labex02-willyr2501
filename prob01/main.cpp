e53f6b6ee831f829efad93788873ecc04508d8f5
willyr2501 <willyr2501@cs104u-3a.acad.fullerton.edu>
2019-08-31 14:25:03 -0700

    short description of code changes being uploaded

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,17 @@
-// Name:
+// Name:Willy Reyes-Rendon
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
+std::cout <<"   *\n";
+std::cout <<"  ***\n";
+std::cout <<" *****\n";
+std::cout<<"*******\n";
+std::cout <<" *****\n";
+std::cout  <<"  ***\n";
+std::cout   <<"   *\n";
 
   return 0;
 }
